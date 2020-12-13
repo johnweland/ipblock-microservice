cc35a76ab83c600d1978d08bfd55bfc7d0cfcba9
John Weland <jweland@icsolutions.com>
2020-12-13 16:40:10 -0600

    build documentation with typedoc

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 const slsw = require('serverless-webpack');
 const nodeExternals = require('webpack-node-externals');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
-const JsDocPlugin = require('jsdoc-webpack-plugin-v2');
 
 module.exports = {
   context: __dirname,
